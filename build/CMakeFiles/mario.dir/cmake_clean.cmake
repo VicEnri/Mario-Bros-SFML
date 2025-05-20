bd47d34ea038f7e70751b41f53fd373561f79561
VicEnri <enricovicale99@gmail.com>
2025-05-20 17:26:36 +0200

    setup

---
+++ build/CMakeFiles/mario.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mario.dir/src/Main.cpp.o"
+  "CMakeFiles/mario.dir/src/Main.cpp.o.d"
+  "mario"
+  "mario.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mario.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ headers/Global.h
@@ -0,0 +1,6 @@
+#pragma once
+
+constexpr const char* TITLE = "Mario Bros";
+constexpr unsigned SCREEN_WIDTH = 800;
+constexpr unsigned SCREEN_HEIGHT = 600;
+constexpr float FRAMERATE = 60;
